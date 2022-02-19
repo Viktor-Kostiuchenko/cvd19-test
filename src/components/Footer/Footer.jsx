fd31d92fd18f0302b9494775300fdcbf0b12615c
Viktor Kostiuchenko <81323303+Viktor-Kostiuchenko@users.noreply.github.com>
2022-02-19 03:47:09 +0200

    Create footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p className="footerText">Designed by Viktor Kost for Boosters with</p>
+      <FavoriteIcon/>
+    </div>
+  )
+  
+}
